780e6331d26b9effcf761d260122cdd7cd730fc9
NOGUEN <nhg1113@naver.com>
2023-09-24 16:41:42 +0900

    fix: fix text alignment issue in newfit login button

--- lib/app/view/common/newfit_button.dart
+++ lib/app/view/common/newfit_button.dart
@@ -57,14 +57,10 @@ class NewfitLoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-      ),
-      child: SizedBox(
-        width: 320.w,
-        height: 40.h,
-        child: ElevatedButton(
+    return SizedBox(
+      width: 320.w,
+      height: 40.h,
+      child: ElevatedButton(
           onPressed: onPressFuntion,
           style: ButtonStyle(
             shape: MaterialStateProperty.all(
@@ -76,20 +72,14 @@ class NewfitLoginButton extends StatelessWidget {
             backgroundColor: MaterialStateProperty.all(buttonColor),
             elevation: MaterialStateProperty.all(0.0),
           ),
-          child: Row(children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  child: buttonLeadingIcon),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Text(buttonText),
-            ),
-          ]),
-        ),
-      ),
+          child: Stack(
+            children: [
+              Align(alignment: Alignment.centerLeft, child: buttonLeadingIcon),
+              Center(
+                child: Text(buttonText),
+              )
+            ],
+          )),
     );
   }
 }
