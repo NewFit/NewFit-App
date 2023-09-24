46f5293ec65fc05f763872e208a8fc87c0affa69
NOGUEN <nhg1113@naver.com>
2023-09-24 16:05:17 +0900

    feat: add newfit login button

--- lib/app/view/common/newfit_button.dart
+++ lib/app/view/common/newfit_button.dart
@@ -38,6 +38,58 @@ class NewfitButton extends StatelessWidget {
         ),
       ),
     );
-    ;
+  }
+}
+
+class NewfitLoginButton extends StatelessWidget {
+  const NewfitLoginButton({
+    super.key,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.buttonLeadingIcon,
+    required this.onPressFuntion,
+  });
+
+  final String buttonText;
+  final Color buttonColor;
+  final Icon buttonLeadingIcon;
+  final Function() onPressFuntion;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      child: SizedBox(
+        width: 320.w,
+        height: 40.h,
+        child: ElevatedButton(
+          onPressed: onPressFuntion,
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+            ),
+            overlayColor: MaterialStateProperty.all(Colors.transparent),
+            backgroundColor: MaterialStateProperty.all(buttonColor),
+            elevation: MaterialStateProperty.all(0.0),
+          ),
+          child: Row(children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: buttonLeadingIcon),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(buttonText),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
