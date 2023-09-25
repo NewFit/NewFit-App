8d4f6511a2c88acbcb10a8283db056f2e9c22f98
NOGUEN <65299607+NOGUEN@users.noreply.github.com>
2023-09-25 13:29:17 +0900

    feat: Component/NewfitButton, Component/NewfitLoginButton  (#6)
    * feat: add newfit button
    
    * feat: add newfit login button
    
    * fix: fix text alignment issue in newfit login button
    
    기본 컴포넌트라 우선 병합했습니다.

---
+++ lib/app/view/common/newfit_button.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NewfitButton extends StatelessWidget {
+  const NewfitButton({
+    super.key,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.onPressFuntion,
+  });
+
+  final String buttonText;
+  final Color buttonColor;
+  final Function() onPressFuntion;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      child: SizedBox(
+        width: 320.w,
+        height: 40.h,
+        child: ElevatedButton(
+          onPressed: onPressFuntion,
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+            ),
+            overlayColor: MaterialStateProperty.all(Colors.transparent),
+            backgroundColor: MaterialStateProperty.all(buttonColor),
+            elevation: MaterialStateProperty.all(0.0),
+          ),
+          child: Text(buttonText),
+        ),
+      ),
+    );
+  }
+}
+
+class NewfitLoginButton extends StatelessWidget {
+  const NewfitLoginButton({
+    super.key,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.buttonLeadingIcon,
+    required this.onPressFuntion,
+  });
+
+  final String buttonText;
+  final Color buttonColor;
+  final Icon buttonLeadingIcon;
+  final Function() onPressFuntion;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 320.w,
+      height: 40.h,
+      child: ElevatedButton(
+          onPressed: onPressFuntion,
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+            ),
+            overlayColor: MaterialStateProperty.all(Colors.transparent),
+            backgroundColor: MaterialStateProperty.all(buttonColor),
+            elevation: MaterialStateProperty.all(0.0),
+          ),
+          child: Stack(
+            children: [
+              Align(alignment: Alignment.centerLeft, child: buttonLeadingIcon),
+              Center(
+                child: Text(buttonText),
+              )
+            ],
+          )),
+    );
+  }
+}
