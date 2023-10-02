e34533befb79efe3f92bbc402920fc69bf6c999d
NOGUEN <nhg1113@naver.com>
2023-10-02 16:22:42 +0900

    feat: add newfit routine equipment list cell

---
+++ lib/app/view/common/newfit_routine_equipmentlist.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_fontweight.dart';
+
+class NewfitRoutineEquipmentListCell extends StatelessWidget {
+  const NewfitRoutineEquipmentListCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 320.w,
+      height: 60.h,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.r),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.black.withOpacity(0.1),
+              offset: const Offset(0, 0),
+              spreadRadius: 0.1,
+              blurRadius: 20,
+            ),
+          ]),
+      child: Row(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                SizedBox(width: 10.w),
+                SizedBox(
+                  width: 50.w,
+                  height: 50.w,
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(8.r)),
+                        child: const Image(
+                          image: AssetImage('images/gorani.png'),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: AppColors.main,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(8.r),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 14.w),
+                Text(
+                  "gorani",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: AppFontWeights.medium,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 30.w, 0),
+              child: Text(
+                '20분',
+                style: TextStyle(
+                  color: AppColors.black,
+                  fontWeight: AppFontWeights.regular,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
