b1566bf9f0c5979f01e4ae073053f2d966755535
NOGUEN <nhg1113@naver.com>
2023-10-05 16:22:37 +0900

    fix: refactor newfitappbarwithbutton class

--- lib/app/view/common/newfit_appbar.dart
+++ lib/app/view/common/newfit_appbar.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:new_fit/app/view/common/newfit_button.dart';
+import 'package:new_fit/app/view/common/newfit_progressbar.dart';
 import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_text_theme.dart';
 
 class NewfitAppBarWithButton extends StatelessWidget
     implements PreferredSizeWidget {
@@ -14,7 +17,8 @@ class NewfitAppBarWithButton extends StatelessWidget
 
   ScrollController scrollController;
   Rx<double> scrollPosition = 0.0.obs;
-  late Widget creditInfo = creditInfoColumn();
+  late Widget creditInfo =
+      _UserCreditInfo(totalCredit: 10000, todayCredit: 100);
   double appBarHeight = 183.h;
 
   @override
@@ -27,7 +31,7 @@ class NewfitAppBarWithButton extends StatelessWidget
         creditInfo = const SizedBox();
       } else {
         appBarHeight = 175.h + MediaQuery.of(context).padding.top;
-        creditInfo = creditInfoColumn();
+        creditInfo = _UserCreditInfo(totalCredit: 10000, todayCredit: 100);
       }
       return Container(
           height: appBarHeight,
@@ -52,12 +56,21 @@ class NewfitAppBarWithButton extends StatelessWidget
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  userInfoAppBar(),
-                  creditInfo,
+                  _UserInfoAppBar(
+                    userName: "고라니",
+                    onPressedFucntion: () {},
+                  ),
+                  _UserCreditInfo(
+                    totalCredit: 10000,
+                    todayCredit: 100,
+                  ),
                   SizedBox(height: 15.h),
                   Align(
                     alignment: Alignment.center,
-                    child: routineButton(),
+                    child: NewfitButton(
+                        buttonText: "루틴으로 예약하기",
+                        buttonColor: AppColors.main,
+                        onPressFuntion: () {}),
                   )
                 ],
               ),
@@ -66,100 +79,94 @@ class NewfitAppBarWithButton extends StatelessWidget
     });
   }
 
-  Widget userInfoAppBar() {
-    return Row(children: [
-      CircleAvatar(
-        radius: 15.h,
-      ),
-      SizedBox(
-        width: 10.w,
-      ),
-      Text("고라니 님"),
-      const Spacer(),
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.settings),
-        iconSize: 24.w,
-      )
-    ]);
-  }
-
-  Widget creditInfoColumn() {
-    return Column(
-      children: [
-        SizedBox(height: 13.h),
-        Row(children: [
-          Text(
-            "전체 크레딧",
-            style: TextStyle(fontSize: 12.sp),
-          ),
-          Text(
-            "10000",
-            style: TextStyle(fontSize: 12.sp),
-          ),
-        ]),
-        SizedBox(height: 7.h),
-        Row(
-          children: [
-            Text(
-              "전체 크레딧",
-              style: TextStyle(fontSize: 12.sp),
-            ),
-            Text(
-              "10000",
-              style: TextStyle(fontSize: 12.sp),
-            ),
-          ],
-        ),
-        SizedBox(height: 15.h),
-        Align(
-          alignment: Alignment.center,
-          child: creditProgressBar(),
-        ),
-      ],
-    );
-  }
-
-  Widget creditProgressBar() {
-    return SizedBox(
-      width: 320.w,
-      child: const LinearProgressIndicator(
-        backgroundColor: Colors.black38,
-        value: 0.5,
-      ),
-    );
-  }
-
-  Widget routineButton() {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: SizedBox(
-        width: 320.w,
-        height: 40.h,
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.r),
-              ),
-            ),
-            overlayColor: MaterialStateProperty.all(Colors.transparent),
-            backgroundColor: MaterialStateProperty.all(AppColors.primaryColor),
-            elevation: MaterialStateProperty.all(0.0),
-          ),
-          child: Text("루틴으로 예약하기"),
-        ),
-      ),
-    );
-  }
-
   _scrollListener() {
     scrollPosition.value = scrollController.offset;
   }
 
   @override
   Size get preferredSize => Size.fromHeight(appBarHeight);
+}
+
+class _UserInfoAppBar extends StatelessWidget {
+  const _UserInfoAppBar({
+    required this.userName,
+    required this.onPressedFucntion,
+    super.key,
+  });
+  final String userName;
+  final Function()? onPressedFucntion;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      CircleAvatar(
+        radius: 15.h,
+        backgroundImage: AssetImage('images/gorani.png'),
+      ),
+      SizedBox(width: 10.w),
+      Text(
+        "$userName님",
+        style: AppTextTheme.bold_lg,
+      ),
+      const Spacer(),
+      IconButton(
+        onPressed: onPressedFucntion,
+        icon: const Icon(Icons.settings),
+        iconSize: 24.w,
+      )
+    ]);
+  }
+}
+
+class _UserCreditInfo extends StatelessWidget {
+  const _UserCreditInfo({
+    required this.totalCredit,
+    required this.todayCredit,
+    super.key,
+  });
+
+  final int totalCredit;
+  final int todayCredit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 13.h),
+        Row(children: [
+          Text(
+            "전체 크레딧",
+            style: AppTextTheme.medium_md,
+          ),
+          SizedBox(width: 5.w),
+          Text(
+            "$totalCredit",
+            style: AppTextTheme.regular_md,
+          ),
+        ]),
+        SizedBox(height: 7.h),
+        Row(
+          children: [
+            Text(
+              "일일 크레딧",
+              style: AppTextTheme.medium_md,
+            ),
+            SizedBox(width: 5.w),
+            Text(
+              "$todayCredit",
+              style: AppTextTheme.regular_md,
+            ),
+          ],
+        ),
+        SizedBox(height: 15.h),
+        Align(
+          alignment: Alignment.center,
+          child: NewfitProgressBar(
+            progressBarHeight: 4.h,
+            progressBarValue: 0.3,
+          ),
+        ),
+      ],
+    );
+  }
 }
