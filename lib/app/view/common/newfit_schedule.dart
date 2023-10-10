4c62ba5d2becf902f91fbfcbb41f208e8787ea0a
노근 <noguen@jeoyo.localdomain>
2023-10-10 20:32:22 +0900

    feat: add current time on scheduler

--- lib/app/view/common/newfit_schedule.dart
+++ lib/app/view/common/newfit_schedule.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:new_fit/app/data/model/reservation_model.dart';
 import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_text_theme.dart';
 
 class NewfitSchedule extends StatelessWidget {
   NewfitSchedule({
@@ -18,7 +20,7 @@ class NewfitSchedule extends StatelessWidget {
             .inMinutes;
     List<Widget> activatedScheduleWidget = [
       Container(
-        width: 320.w,
+        width: 300.w,
         height: 15.h,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(4.r),
@@ -45,12 +47,28 @@ class NewfitSchedule extends StatelessWidget {
       );
     }
 
-    return Stack(children: activatedScheduleWidget);
+    return SizedBox(
+      width: 320.w,
+      child: Column(children: [
+        Row(children: [
+          NewfitTextRegularXs(
+            text: "${DateFormat('h:mm').format(DateTime.now())}",
+            textColor: AppColors.unabledGrey,
+          ),
+          Spacer(),
+          NewfitTextRegularXs(
+            text: "오후 4시",
+            textColor: AppColors.unabledGrey,
+          ),
+        ]),
+        Stack(children: activatedScheduleWidget),
+      ]),
+    );
   }
 }
 
 class _NewfitScheduleActivatedArea extends StatelessWidget {
-  const _NewfitScheduleActivatedArea({
+  _NewfitScheduleActivatedArea({
     required this.areaWidth,
     required this.startPosition,
     required this.appColor,
@@ -58,19 +76,41 @@ class _NewfitScheduleActivatedArea extends StatelessWidget {
   });
 
   final double startPosition;
-  final double areaWidth;
+  double areaWidth;
   final Color appColor;
 
   @override
   Widget build(BuildContext context) {
+    BorderRadius border = BorderRadius.only(
+      topLeft: Radius.circular(4.r),
+      bottomLeft: Radius.circular(4.r),
+      topRight: Radius.circular(4.r),
+      bottomRight: Radius.circular(4.r),
+    );
+
+    if (startPosition <= 5) {
+      border = BorderRadius.only(
+        topLeft: Radius.circular(4.r),
+        bottomLeft: Radius.circular(4.r),
+      );
+    }
+
+    if (startPosition + areaWidth * 2.5 >= 320) {
+      areaWidth -= (startPosition + areaWidth) - 320;
+      border = BorderRadius.only(
+        topRight: Radius.circular(4.r),
+        bottomRight: Radius.circular(4.r),
+      );
+    }
     return Positioned(
-      left: startPosition,
+      left: startPosition.w,
       child: GestureDetector(
         onTap: () {},
         child: Container(
-          width: areaWidth,
+          width: areaWidth.w * 2.5,
           height: 15.h,
           decoration: BoxDecoration(
+            borderRadius: border,
             color: appColor,
           ),
         ),
