2ce2525bb3206852919386163244eafb0a247b13
NOGUEN <nhg1113@naver.com>
2023-10-12 21:00:58 +0900

    feat: add newfit lists' base structure. searchlistcell, scoreboardlistcell, settinglistcell

---
+++ lib/app/view/common/newfit_lists.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class NewfitSearchListCell extends StatelessWidget {
+  const NewfitSearchListCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NewfitScoreboardListCell extends StatelessWidget {
+  const NewfitScoreboardListCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NewfitSettingListCell extends StatelessWidget {
+  const NewfitSettingListCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
