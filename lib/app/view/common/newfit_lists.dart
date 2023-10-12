fab2bd221573e121673da470df8cbfe9695265fb
NOGUEN <nhg1113@naver.com>
2023-10-12 21:54:29 +0900

    feat: add toggle feature

--- lib/app/view/common/newfit_lists.dart
+++ lib/app/view/common/newfit_lists.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_text_theme.dart';
 
 class NewfitSearchListCell extends StatelessWidget {
-  const NewfitSearchListCell({super.key});
+  NewfitSearchListCell({
+    required this.gymTitleText,
+    required this.gymLocationText,
+    super.key,
+  });
+
+  final String gymTitleText;
+  final String gymLocationText;
+  Rx<bool> toggled = false.obs;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.only(bottom: 3.h),
+      child: Obx(() {
+        return GestureDetector(
+          child: Container(
+            width: double.infinity,
+            height: toggled.value ? 100.h : 56.h,
+            decoration: const BoxDecoration(color: AppColors.white),
+            child: Padding(
+              padding: EdgeInsets.only(left: 15.w, right: 15.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  NewfitTextMediumMd(
+                    text: gymTitleText,
+                    textColor: AppColors.black,
+                  ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        size: 12.w,
+                        color: AppColors.textUnabled,
+                      ),
+                      NewfitTextRegularMd(
+                        text: gymLocationText,
+                        textColor: AppColors.textUnabled,
+                      )
+                    ],
+                  ),
+                  toggled.value
+                      ? Container(
+                          height: 30,
+                          decoration: BoxDecoration(color: Colors.red),
+                        )
+                      : SizedBox(),
+                  const Spacer(),
+                ],
+              ),
+            ),
+          ),
+          onTap: () {
+            toggled.value = !toggled.value;
+          },
+        );
+      }),
+    );
   }
 }
 
