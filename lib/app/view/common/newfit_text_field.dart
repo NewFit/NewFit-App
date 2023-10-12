906b6880ccd61cb3ad6251cfc45b160913de3593
NOGUEN <65299607+NOGUEN@users.noreply.github.com>
2023-10-12 20:56:29 +0900

    feat: Component/NewfitTextfields (#24)
    * feat: add newfit search text field and newfit inof input text field base structure
    
    * feat: add newfitidinputtextfield and newfitpasswordinputtextfield which overrides newfitinfoinput textfield

---
+++ lib/app/view/common/newfit_text_field.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_fit/app/view/theme/app_colors.dart';
+
+class NewfitInfoInputTextField extends StatelessWidget {
+  const NewfitInfoInputTextField({
+    required this.hintText,
+    super.key,
+  });
+
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 320.w,
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          contentPadding:
+              EdgeInsets.only(left: 10.w, top: 10.h, bottom: 10.h, right: 0),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide.none,
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide(
+              color: AppColors.warning,
+              width: 1.w,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide(
+              color: AppColors.warning,
+              width: 1.w,
+            ),
+          ),
+          fillColor: AppColors.white,
+          filled: true,
+          hintText: hintText,
+        ),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: (value) {
+          return _validator(value!);
+        },
+      ),
+    );
+  }
+
+  String? _validator(String value) {
+    return null;
+  }
+}
+
+class NewfitIdInputTextField extends NewfitInfoInputTextField {
+  NewfitIdInputTextField({required super.hintText});
+
+  @override
+  String? _validator(String value) {
+    if (value.length < 10) {
+      return ('아이디에는 특수기호를 사용할 수 없습니다.');
+    }
+  }
+}
+
+class NewfitPasswordInputTextField extends NewfitInfoInputTextField {
+  NewfitPasswordInputTextField({required super.hintText});
+
+  @override
+  String? _validator(String value) {
+    if (value.length <= 7) {
+      return ('비밀번호를 더 길게 설정해주세요.');
+    }
+  }
+}
+
+class NewfitSearchTextField extends StatelessWidget {
+  const NewfitSearchTextField({
+    required this.hintText,
+    super.key,
+  });
+
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 320.w,
+      child: TextField(
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 10.h),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide(
+              color: AppColors.blue500,
+              width: 2.w,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7.r),
+            borderSide: BorderSide(
+              color: AppColors.blue500,
+              width: 2.w,
+            ),
+          ),
+          hintText: '주소를 입력해주세요.',
+          prefixIcon: const Icon(
+            Icons.search,
+            color: AppColors.black,
+          ),
+          suffixIcon: const Icon(
+            Icons.close,
+            color: AppColors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
