f5fc3414151bce0570524603568cc835bd4cb210
NOGUEN <nhg1113@naver.com>
2023-10-22 21:48:37 +0900

    feat: add base page settings

---
+++ lib/app/view/my_page/my_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/preferred_size.dart';
+import 'package:new_fit/app/controller/my_page_controller.dart';
+import 'package:new_fit/app/core/base/base_view.dart';
+import 'package:new_fit/app/view/common/base_body.dart';
+
+class MyPage extends BaseView<MyPageController> {
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return BaseBody(widgetList: []);
+  }
+
+  Widget UserInfoTab() {
+    return Stack(
+      children: [
+        SizedBox(),
+      ],
+    );
+  }
+}
