2a43b7c82fb09c7fefd9282a7af5668d6896f2cf
NOGUEN <nhg1113@naver.com>
2023-10-27 15:48:57 +0900

    feat: add scoreboard page

--- lib/app/view/common/newfit_circle_avatar.dart
+++ lib/app/view/common/newfit_circle_avatar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_text_theme.dart';
 
 class NewfitCircleAvatar extends StatelessWidget {
   const NewfitCircleAvatar({
@@ -11,8 +13,24 @@ class NewfitCircleAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 20.h,
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: radius,
+          ),
+          SizedBox(height: 15.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.circle),
+              SizedBox(width: 5.w),
+              NewfitTextBoldXl(text: 'gorani', textColor: AppColors.black),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
