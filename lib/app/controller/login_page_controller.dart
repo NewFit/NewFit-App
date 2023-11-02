4243c6bb9caac807ac5bb33fa47ae57acbaf9681
NOGUEN <nhg1113@naver.com>
2023-11-02 22:58:17 +0900

    feat: add login_page_controller

--- lib/app/controller/login_page_controller.dart
+++ lib/app/controller/login_page_controller.dart
@@ -1,3 +1,15 @@
+import 'package:new_fit/app/controller/service/google_login.dart';
+import 'package:new_fit/app/controller/service/kakao_login.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 
-class LoginPageController extends BaseController {}
+class LoginPageController extends BaseController {
+  bool loginSuccess = false;
+
+  Future<bool> kakaoLogin() async {
+    return await KakaoLogin().login();
+  }
+
+  Future<bool> googleLogin() async {
+    return await GoogleLogin().login();
+  }
+}
