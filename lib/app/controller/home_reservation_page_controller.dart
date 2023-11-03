e513a625622bf87cafd39f029042230e95c84314
NOGUEN <nhg1113@naver.com>
2023-11-03 16:16:59 +0900

    fix: fix package route and reservation parameter

--- lib/app/controller/home_reservation_page_controller.dart
+++ lib/app/controller/home_reservation_page_controller.dart
@@ -2,8 +2,7 @@ import 'dart:math';
 
 import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
-
-import '../data/model/reservation_model.dart';
+import 'package:new_fit/app/data/model/json_models/reservation/reservation_model.dart';
 
 class HomeReservationPageController extends BaseController {
   //final RxList<Reservation> reservations = ;
@@ -19,13 +18,12 @@ class HomeReservationPageController extends BaseController {
     for (int i = 0; i < 4; i++) {
       int randomMinuteStart = random.nextInt(totalPeriodInMinutes);
       int randomDuration =
-          random.nextInt(totalPeriodInMinutes - randomMinuteStart) +
-              1;
+          random.nextInt(totalPeriodInMinutes - randomMinuteStart) + 1;
 
       DateTime start = nearestHour.add(Duration(minutes: randomMinuteStart));
       DateTime end = start.add(Duration(minutes: randomDuration));
 
-      reservations.add(Reservation(startAt: start, endAt: end));
+      reservations.add(Reservation(start_at: start, end_at: end));
     }
 
     return reservations;
