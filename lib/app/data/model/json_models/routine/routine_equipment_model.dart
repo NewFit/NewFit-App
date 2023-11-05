d48fc137a6981890f86b91105202828dc63b3284
NOGUEN <nhg1113@naver.com>
2023-11-05 18:17:35 +0900

    feat: add routine models

--- lib/app/data/model/json_models/routine/routine_equipment_model.dart
+++ lib/app/data/model/json_models/routine/routine_equipment_model.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:json_annotation/json_annotation.dart';
-import 'package:new_fit/app/data/model/json_models/reservation/reservation_model.dart';
 
 part 'routine_equipment_model.g.dart';
 
