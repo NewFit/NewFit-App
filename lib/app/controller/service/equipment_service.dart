292393c165c9bc7cc6b6010d3ebcc5c2158ea13e
NOGUEN <nhg1113@naver.com>
2023-11-06 15:45:38 +0900

    feat: add equipment service

---
+++ lib/app/controller/service/equipment_service.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:new_fit/app/data/model/json_models/equipment/equipment_list_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'equipment_service.g.dart';
+
+@RestApi(
+    baseUrl:
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/")
+abstract class EquipmentService {
+  factory EquipmentService(Dio dio, {String baseUrl}) = _EquipmentService;
+
+  @GET('/equipments')
+  Future<EquipmentList> getAllEquipmentsInGym();
+
+  @GET('/equipments')
+  Future<EquipmentList> getAllEquipmentsByPurpose(
+      @Query('purpose') String purpose);
+
+  @GET('equipments')
+  Future<EquipmentList> getIdenticalEquipments(
+      @Query('euiqpment_id') int euiqpment_id);
+}
