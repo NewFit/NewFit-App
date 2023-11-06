7de7a8a4062aa208ca0c5477e3313f0893210ab1
NOGUEN <nhg1113@naver.com>
2023-11-06 18:58:20 +0900

    refactor: change directory name

---
+++ lib/app/controller/network_service/gym_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:new_fit/app/data/model/json_models/gym/address_gym_list_model.dart';
+import 'package:new_fit/app/data/model/json_models/mypage/mypage_model.dart';
+import 'package:new_fit/app/data/model/json_models/user/access_token.dart';
+import 'package:new_fit/app/data/model/json_models/user/modify_user_model.dart';
+import 'package:new_fit/app/data/model/json_models/user/user_email_model.dart';
+import 'package:new_fit/app/data/model/json_models/user/user_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'gym_service.g.dart';
+
+@RestApi(
+    baseUrl:
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/")
+abstract class GymService {
+  factory GymService(Dio dio, {String baseUrl}) = _GymService;
+
+  @GET('/gyms')
+  Future<AddressGymList> getGymList(@Query('gym_name') String gym_name);
+}
