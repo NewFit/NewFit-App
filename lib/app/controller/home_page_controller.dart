19f92fbc31fcc3c6407f7b3694fec63637180f59
Haneol <haneolj@gmail.com>
2023-11-09 02:28:00 +0900

    feat: add EquipmentService in HomePageController

--- lib/app/controller/home_page_controller.dart
+++ lib/app/controller/home_page_controller.dart
@@ -1,3 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 
-class HomePageController extends BaseController {}
+import '../data/model/json_models/equipment/equipment_list_model.dart';
+import 'network_service/equipment_service.dart';
+
+class HomePageController extends BaseController {
+  final Dio dio = Dio();
+  late final EquipmentService service;
+
+  @override
+  void onInit() async {
+    service = EquipmentService(dio);
+    super.onInit();
+  }
+
+  Future<EquipmentList> getAllEquipmentsInGym() async {
+    return await service.getAllEquipmentsInGym();
+  }
+
+  @override
+  void onClose() {
+    dio.close();
+    super.onClose();
+  }
+}
