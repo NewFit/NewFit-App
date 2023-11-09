583a8faf97910ae63e40abc2a3b1bc2066114f47
NOGUEN <nhg1113@naver.com>
2023-11-09 20:20:53 +0900

    refactor: User관련 Json 파일들을 합쳤습니다. Token과 AccessToken를 token.dart 파일로, User와 ModifyUser, 그리고 UserEmail을 user.dart로 한 파일로 합쳤습니다.

--- lib/app/data/model/json_models/user/token_model.dart
+++ lib/app/data/model/json_models/user/token_model.dart
@@ -5,12 +5,32 @@ part 'token_model.g.dart';
 @JsonSerializable()
 class Token {
   String access_token;
+  String? refresh_token;
+  int id;
+  String id_type;
 
   Token({
     required this.access_token,
+    required this.refresh_token,
+    required this.id,
+    required this.id_type,
   });
 
   factory Token.fromJson(Map<String, dynamic> json) => _$TokenFromJson(json);
 
   Map<String, dynamic> toJson() => _$TokenToJson(this);
+}
+
+@JsonSerializable()
+class AccessToken {
+  String access_token;
+
+  AccessToken({
+    required this.access_token,
+  });
+
+  factory AccessToken.fromJson(Map<String, dynamic> json) =>
+      _$AccessTokenFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AccessTokenToJson(this);
 }
