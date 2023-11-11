b3399783e52442bab032fad58ac7efc319c1ff43
NOGUEN <nhg1113@naver.com>
2023-11-12 01:18:31 +0900

    feat: checkRegisterStatus로 페이지 이동 로직 변경

--- lib/app/controller/login_page_controller.dart
+++ lib/app/controller/login_page_controller.dart
@@ -1,15 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:new_fit/app/routes/app_pages.dart';
 import 'package:new_fit/app/services/service/google_login.dart';
 import 'package:new_fit/app/services/service/kakao_login.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
+import 'package:new_fit/app/view/theme/app_string.dart';
 
 class LoginPageController extends BaseController {
-  bool loginSuccess = false;
+  late String registerStatus;
 
-  Future<String> kakaoLogin() async {
-    return await KakaoLogin().login();
+  void kakaoLogin() async {
+    registerStatus = await KakaoLogin().login();
+    checkRegisterStatus();
   }
 
-  Future<String> googleLogin() async {
-    return await GoogleLogin().login();
+  void googleLogin() async {
+    registerStatus = await GoogleLogin().login();
+    checkRegisterStatus();
+  }
+
+  void checkRegisterStatus() {
+    if (registerStatus == AppString.notRegistered) {
+      debugPrint("not registered user");
+      Get.toNamed(AppPages.REGISTER);
+    } else if (registerStatus == AppString.registered) {
+      debugPrint("registered user");
+      Get.toNamed(AppPages.INITIAL);
+    } else if (registerStatus == AppString.registeredButNoGym) {
+      debugPrint("registered but gym not registered");
+      Get.toNamed(AppPages.REGISTER);
+    }
   }
 }
