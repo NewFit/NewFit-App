ac520ae95a19fa987fd26e89937c77f179e7fd73
NOGUEN <nhg1113@naver.com>
2023-11-25 14:26:51 +0900

    fix: 임시 액세스 토큰 연결

--- lib/app/controller/register_page_controller.dart
+++ lib/app/controller/register_page_controller.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 import 'package:new_fit/app/data/model/json_models/user/user_model.dart';
 import 'package:new_fit/app/services/network_service/user_service.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class RegisterPageController extends BaseController
     with GetTickerProviderStateMixin {
@@ -27,17 +28,31 @@ class RegisterPageController extends BaseController
   @override
   void onInit() {
     super.onInit();
+
     tabController = TabController(vsync: this, length: 6);
   }
 
   void register() {
+    final logger = PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+      compact: true,
+      maxWidth: 500,
+    );
+
+    dio.interceptors.add(logger);
     UserService(dio).signUp(
-        'jwt-token 자리 보안상의 이유로 올리진 않음',
-        User(
-            username: nameEditingController.text,
-            email: emailEditingController.text,
-            nickname: nicknameEditingController.text,
-            tel: phonenumberEditingController.text));
+      'Bearer 임시 액세스 토큰',
+      User(
+        username: nameEditingController.text,
+        email: emailEditingController.text,
+        nickname: nicknameEditingController.text,
+        tel: phonenumberEditingController.text,
+      ),
+    );
   }
 
   @override
