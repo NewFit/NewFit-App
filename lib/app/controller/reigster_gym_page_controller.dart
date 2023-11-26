5045a7eb05e572e79febdb8d452a881cbc213f7c
NOGUEN <nhg1113@naver.com>
2023-11-26 23:25:19 +0900

    feat: 헬스장 리스트를 받아오는 getAddressGymList 메소드 추가

--- lib/app/controller/reigster_gym_page_controller.dart
+++ lib/app/controller/reigster_gym_page_controller.dart
@@ -1,3 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
+import 'package:new_fit/app/data/model/json_models/gym/gym_model.dart';
+import 'package:new_fit/app/services/network_service/gym_service.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-class RegisterGymPageController extends BaseController {}
+class RegisterGymPageController extends BaseController {
+  TextEditingController textEditingController = TextEditingController();
+  late AddressGymList addressGymList;
+  Dio dio = Dio();
+  final prettyDioLogger = PrettyDioLogger(
+    requestHeader: true,
+    requestBody: true,
+    responseBody: true,
+    responseHeader: true,
+    error: true,
+    compact: true,
+    maxWidth: 80,
+  );
+
+  Future<void> getAddressGymList() async {
+    dio.interceptors.add(prettyDioLogger);
+    try {
+      addressGymList = await GymService(dio).getGymList("gym_name");
+    } catch (error) {
+      error.printError();
+    }
+  }
+}
