eba2b5af0b798486cd368d0ae269b7d987508a3c
NOGUEN <nhg1113@naver.com>
2023-11-26 20:45:43 +0900

    fix: 처음 연결되는 페이지를 로그인 페이지로 수정

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -24,7 +24,7 @@ class _AppState extends State<App> {
       builder: (buildContext, widget) => GetMaterialApp(
         title: _envConfig.appName,
         debugShowCheckedModeBanner: false,
-        initialRoute: AppPages.REGISTER,
+        initialRoute: AppPages.LOGIN,
         getPages: AppPages.pages,
         initialBinding: InitialBinding(),
         theme: appThemeData,
