b6e3793a7eee3f5be02469dd331d524c341a2d7a
NOGUEN <nhg1113@naver.com>
2023-11-26 20:42:06 +0900

    feat: 정수 저장과 읽기 메소드인 saveInt()와 getInt() 메소드 추가

--- lib/app/data/local/db/storage_util.dart
+++ lib/app/data/local/db/storage_util.dart
@@ -7,7 +7,15 @@ mixin StorageUtil {
     await _storage.write(key, uid);
   }
 
+  Future<void> saveInt(String key, int uid) async {
+    await _storage.write(key, uid);
+  }
+
   String? getString(String key) {
+    return _storage.read(key);
+  }
+
+  int? getInt(String key) {
     return _storage.read(key);
   }
 }
