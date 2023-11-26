794544e1c2ca7410bc4483e0ce044a4a9affcfd0
NOGUEN <nhg1113@naver.com>
2023-11-27 00:39:00 +0900

    fix: getAddressGymList 메소드 수정

--- lib/app/controller/reigster_gym_page_controller.dart
+++ lib/app/controller/reigster_gym_page_controller.dart
@@ -2,13 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
+import 'package:new_fit/app/data/local/db/storage_util.dart';
 import 'package:new_fit/app/data/model/json_models/gym/gym_model.dart';
 import 'package:new_fit/app/services/network_service/gym_service.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-class RegisterGymPageController extends BaseController {
+class RegisterGymPageController extends BaseController with StorageUtil {
   TextEditingController textEditingController = TextEditingController();
-  late AddressGymList addressGymList;
+  AddressGymList addressGymList = AddressGymList(gym_count: 0, gyms: []);
   Dio dio = Dio();
   final prettyDioLogger = PrettyDioLogger(
     requestHeader: true,
@@ -20,10 +21,13 @@ class RegisterGymPageController extends BaseController {
     maxWidth: 80,
   );
 
-  Future<void> getAddressGymList() async {
+  Future<void> getAddressGymList(String gymName) async {
     dio.interceptors.add(prettyDioLogger);
     try {
-      addressGymList = await GymService(dio).getGymList("gym_name");
+      addressGymList = await GymService(dio).getGymList(
+          'Bearer ${getString('access-token')!}',
+          getInt('oauth-history-id')!,
+          gymName);
     } catch (error) {
       error.printError();
     }
