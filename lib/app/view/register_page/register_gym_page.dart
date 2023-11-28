652bc47a47bb14dc5934dd151360da675fe3baa4
NOGUEN <nhg1113@naver.com>
2023-11-28 15:31:18 +0900

    fix: 위젯들을 Obx화 시킴

--- lib/app/view/register_page/register_gym_page.dart
+++ lib/app/view/register_page/register_gym_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:new_fit/app/controller/reigster_gym_page_controller.dart';
 import 'package:new_fit/app/core/base/base_view.dart';
 import 'package:new_fit/app/view/common/base_body.dart';
@@ -11,6 +12,21 @@ import 'package:new_fit/app/view/common/newfit_text_field.dart';
 import 'package:new_fit/app/view/theme/app_colors.dart';
 
 class RegisterGymPage extends BaseView<RegisterGymPageController> {
+  @override
+  Widget pageScaffold(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: pageBackgroundColor(),
+      key: globalKey,
+      appBar: appBar(context),
+      floatingActionButton: floatingActionButton(),
+      body: pageContent(context),
+      bottomNavigationBar: bottomNavigationBar(),
+      bottomSheet: bottomSheet(),
+      drawer: drawer(),
+    );
+  }
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return null;
@@ -25,22 +41,42 @@ class RegisterGymPage extends BaseView<RegisterGymPageController> {
           textEditingController: controller.textEditingController,
           onSubmittedFunction: controller.getAddressGymList,
         ),
-        SizedBox(
-          height: 420.h,
-          child: ListView.builder(
-            itemCount: controller.addressGymList.gym_count,
-            itemBuilder: (context, index) {
-              return NewfitSearchListCell(
-                gymTitleText: controller.addressGymList.gyms[index].gym_name,
-                gymLocationText: controller.addressGymList.gyms[index].address,
-              );
-            },
-          ),
+        Obx(
+          () {
+            return SizedBox(
+              height: 420.h,
+              child: ListView.builder(
+                itemCount: controller.addressGymList.value.gym_count,
+                itemBuilder: (context, index) {
+                  return NewfitSearchListCell(
+                    gymTitleText:
+                        controller.addressGymList.value.gyms[index].gym_name,
+                    gymLocationText:
+                        controller.addressGymList.value.gyms[index].address,
+                    onTapFunction: () {
+                      controller.selected.value = !controller.selected.value;
+                    },
+                  );
+                },
+              ),
+            );
+          },
         ),
-        NewfitButton(
-          buttonText: '등록 요청하기',
-          buttonColor: AppColors.main,
-          onPressFuntion: () {},
+        Obx(
+          () {
+            Color buttonColor = Colors.grey;
+
+            if (controller.selected.value) {
+              buttonColor = AppColors.main;
+            } else {
+              buttonColor = Colors.grey;
+            }
+            return NewfitButton(
+              buttonText: '등록 요청하기',
+              buttonColor: AppColors.main,
+              onPressFuntion: () {},
+            );
+          },
         ),
         Padding(
           padding: EdgeInsets.symmetric(vertical: 10.h),
