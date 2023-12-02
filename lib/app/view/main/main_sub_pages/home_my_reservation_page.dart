7bf99b0328c75a56a383753118d1445e73c66ff7
NOGUEN <nhg1113@naver.com>
2023-12-03 01:35:35 +0900

    chore: 예약 현황 페이지 기본 세팅

---
+++ lib/app/view/main/main_sub_pages/home_my_reservation_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/preferred_size.dart';
+import 'package:new_fit/app/controller/home_my_reservation_page_controller.dart';
+import 'package:new_fit/app/core/base/base_view.dart';
+import 'package:new_fit/app/view/common/base_body.dart';
+
+class HomeMyReservationPage extends BaseView<HomeMyReservationPageController> {
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return BaseBody(widgetList: []);
+  }
+}
