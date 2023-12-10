1c4518bef53bd1f582e305493b5e761790aae532
NOGUEN <nhg1113@naver.com>
2023-12-10 13:41:05 +0900

    fix: 빠진 헤더 추가. 카테고리에 맞지 않는 메소드 제거.

--- lib/app/services/network_service/routine_service.dart
+++ lib/app/services/network_service/routine_service.dart
@@ -9,26 +9,36 @@ part 'routine_service.g.dart';
 
 @RestApi(
     baseUrl:
-        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/")
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/api/v1")
 abstract class RoutineService {
   factory RoutineService(Dio dio, {String baseUrl}) = _RoutineService;
 
   @GET('/routines')
-  Future<MyRoutineList> getMyRoutineList();
+  Future<MyRoutineList> getMyRoutineList(
+    @Header('authority-id') int authorityId,
+    @Header('Authorization') String accessToken,
+  );
 
   @POST('/routines')
-  Future<HttpResponse> addRoutine(@Body() PostRoutine postRoutine);
+  Future<HttpResponse> addRoutine(
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Body() PostRoutine postRoutine);
 
   @PATCH('/routines/{routine_id}')
   Future<HttpResponse> editRoutine(
-      @Path() int routine_id, @Body() PatchRoutine patchRoutine);
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Path() int routine_id,
+      @Body() PatchRoutine patchRoutine);
 
   @DELETE('/routines')
-  Future<HttpResponse> deleteRoutine(@Body() RoutineId routineId);
+  Future<HttpResponse> deleteRoutine(@Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken, @Body() RoutineId routineId);
 
   @GET('/routines/{routine_id}')
-  Future<RoutineDetail> getMyRoutineDetails(@Path() int routine_id);
-
-  @POST('/routines/{routine_id}')
-  Future<HttpResponse> reserveWithRoutine(@Body() StartAt startAt);
+  Future<RoutineDetail> getMyRoutineDetails(
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Path() int routine_id);
 }
