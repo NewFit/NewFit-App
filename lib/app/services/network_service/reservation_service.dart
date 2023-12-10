a2fad31bab963299bc1e911dec1d121c4ecd1ff5
NOGUEN <nhg1113@naver.com>
2023-12-10 13:22:19 +0900

    refactor: 루틴으로 예약 메소드를 reservations 파일로 이동.

--- lib/app/services/network_service/reservation_service.dart
+++ lib/app/services/network_service/reservation_service.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:new_fit/app/data/model/json_models/reservation/reservation_models.dart';
+import 'package:new_fit/app/data/model/json_models/tag/equipment_tag_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'reservation_service.g.dart';
 
 @RestApi(
     baseUrl:
-        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/api/v1")
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/api/v1/")
 abstract class ReservationService {
   factory ReservationService(Dio dio, {String baseUrl}) = _ReservationService;
 
@@ -24,5 +25,20 @@ abstract class ReservationService {
   Future<HttpResponse> deleteReservation(@Path() int reservationId);
 
   @GET('/reservations/{reservation_id}')
-  Future<SpecificReservation> getSpecificReservation(@Path() int reservationId);
+  Future<SpecificReservation> getSpecificReservation(
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Path() int reservationId);
+
+  @PATCH('/reservations/start')
+  Future<HttpResponse> startUsingEquipment(
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Body() EquipmentTag equipmentTag);
+
+  @POST('reservations/routines/{routine_id}')
+  Future<HttpResponse> reserveWithRoutine(
+      @Header('authority-id') int authorityId,
+      @Header('Authorization') String accessToken,
+      @Body() StartAt startAt);
 }
