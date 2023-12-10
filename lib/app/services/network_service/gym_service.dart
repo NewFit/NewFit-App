0ef9becb72728a432b7e6271c0168e5d93bc2dd0
NOGUEN <nhg1113@naver.com>
2023-12-10 13:11:42 +0900

    refactor: 불필요한 Import제거. 카테고리와 맞지 않은 메소드 제거. 변수 이름 camelCase로 수정.

--- lib/app/services/network_service/gym_service.dart
+++ lib/app/services/network_service/gym_service.dart
@@ -1,21 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:new_fit/app/data/model/json_models/gym/gym_model.dart';
-import 'package:new_fit/app/data/model/json_models/user/token_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'gym_service.g.dart';
 
 @RestApi(
     baseUrl:
-        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/")
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/avi/v1")
 abstract class GymService {
   factory GymService(Dio dio, {String baseUrl}) = _GymService;
 
-  @GET('/api/v1/gyms')
+  @GET('/gyms')
   Future<AddressGymList> getGymList(@Header('Authorization') String accessToken,
-      @Header('user-id') int user_id, @Query('gym_name') String gym_name);
-
-  @POST('/api/v1/authority')
-  Future<void> registerGym(@Header('user-id') int user_id,
-      @Header('Authorization') String accessToken, @Body() GymId gym_id);
+      @Header('user-id') int userId, @Query('gym_name') String gymName);
 }
