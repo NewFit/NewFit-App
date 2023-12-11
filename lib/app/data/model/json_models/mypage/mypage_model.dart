85324d854d7eb0da40e3cc2d2a6bdc417c5fc1e0
NOGUEN <nhg1113@naver.com>
2023-12-12 00:13:04 +0900

    feat: MyPageInfoNotRegisterd 헬스장 등록이 되지 않은 유저를 위한 모델 추가

--- lib/app/data/model/json_models/mypage/mypage_model.dart
+++ lib/app/data/model/json_models/mypage/mypage_model.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:json_annotation/json_annotation.dart';
+import 'package:new_fit/app/data/model/json_models/gym/gym_model.dart';
 
 part 'mypage_model.g.dart';
 
@@ -10,22 +11,42 @@ class MyPageInfo {
   String nickname;
   int total_credit;
   int this_month_credit;
-  //AuthorityGym current;
-  //int authority_gyms_count;
-  //List<AuthorityGym> authority_gyms;
+  AuthorityGym current;
+  int authority_gyms_count;
+  List<AuthorityGym> authority_gyms;
 
   MyPageInfo({
     required this.profile_file_path,
     required this.nickname,
     required this.total_credit,
     required this.this_month_credit,
-    //required this.current,
-    //required this.authority_gyms_count,
-    //required this.authority_gyms,
+    required this.current,
+    required this.authority_gyms_count,
+    required this.authority_gyms,
   });
 
   factory MyPageInfo.fromJson(Map<String, dynamic> json) =>
       _$MyPageInfoFromJson(json);
 
   Map<String, dynamic> toJson() => _$MyPageInfoToJson(this);
+}
+
+@JsonSerializable()
+class MyPageInfoNotRegistered {
+  String profile_file_path;
+  String nickname;
+  int total_credit;
+  int this_month_credit;
+
+  MyPageInfoNotRegistered({
+    required this.profile_file_path,
+    required this.nickname,
+    required this.total_credit,
+    required this.this_month_credit,
+  });
+
+  factory MyPageInfoNotRegistered.fromJson(Map<String, dynamic> json) =>
+      _$MyPageInfoNotRegisteredFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MyPageInfoNotRegisteredToJson(this);
 }
