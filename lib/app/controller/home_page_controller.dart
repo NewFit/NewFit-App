0d4b5d8046f2a73901d1af687641a409b9deb0bb
Haneol <haneolj@gmail.com>
2023-12-12 12:46:52 +0900

    feat: connect with equipment reservation service

--- lib/app/controller/home_page_controller.dart
+++ lib/app/controller/home_page_controller.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 import 'package:new_fit/app/data/local/db/storage_util.dart';
+import 'package:new_fit/app/data/model/json_models/tag/entrance_tag_model.dart';
+import 'package:new_fit/app/services/network_service/authority_service.dart';
 import 'package:new_fit/app/view/theme/app_string.dart';
 
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -12,7 +14,8 @@ import '../services/network_service/equipment_service.dart';
 
 class HomePageController extends BaseController with StorageUtil {
   final Dio dio = Dio();
-  late final EquipmentService service;
+  late final EquipmentService equipmentService;
+  late final AuthorityService authorityService;
 
   var equipmentList = Rx<EquipmentList?>(null);
   var isLoading = true.obs;
@@ -23,6 +26,8 @@ class HomePageController extends BaseController with StorageUtil {
 
     initService();
     loadEquipments();
+
+    authorityEntry();
   }
 
   void initService() {
@@ -37,7 +42,8 @@ class HomePageController extends BaseController with StorageUtil {
     );
 
     dio.interceptors.add(logger);
-    service = EquipmentService(dio);
+    equipmentService = EquipmentService(dio);
+    authorityService = AuthorityService(dio);
   }
 
   void loadEquipments() async {
@@ -50,13 +56,29 @@ class HomePageController extends BaseController with StorageUtil {
       if (authorityId != null) {
         log('authority id is $authorityId');
         var equipments =
-            await service.getAllEquipmentsInGym(authorityId, token);
+            await equipmentService.getAllEquipmentsInGym(authorityId, token);
         equipmentList(equipments);
       } else {
         log('ERROR : authority id is null!');
       }
     } finally {
       isLoading(false);
+    }
+  }
+
+  //TODO : 실제 GPS와 연동해야함? 일회성 or 필요할 때마다?
+  void authorityEntry() async {
+    final token =
+        '${AppString.jwt_prefix} ${getString(AppString.key_access_token)}';
+    final authorityId = getInt(AppString.key_authority_id);
+    final gymId = getInt(AppString.key_gym_id)!;
+
+    if (authorityId != null) {
+      log('authority id is $authorityId');
+      await authorityService.enterGym(authorityId, token,
+          EntranceTag(gym_id: gymId, tag_at: DateTime.now()));
+    } else {
+      log('ERROR : authority id is null!');
     }
   }
 
