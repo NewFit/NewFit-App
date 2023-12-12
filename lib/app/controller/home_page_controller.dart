1ae8b851d0d105649f8ce231416951fac2716eee
NOGUEN <nhg1113@naver.com>
2023-12-12 14:32:02 +0900

    fix: 유저 정보를 불러오는 로직 재추가

--- lib/app/controller/home_page_controller.dart
+++ lib/app/controller/home_page_controller.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 import 'package:new_fit/app/data/local/db/storage_util.dart';
+import 'package:new_fit/app/data/model/json_models/mypage/mypage_model.dart';
+import 'package:new_fit/app/services/network_service/user_service.dart';
 import 'package:new_fit/app/view/theme/app_string.dart';
 
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -23,6 +25,7 @@ class HomePageController extends BaseController with StorageUtil {
 
     initService();
     loadEquipments();
+    loadMyPageInfo();
   }
 
   void initService() {
@@ -64,5 +67,34 @@ class HomePageController extends BaseController with StorageUtil {
   void onClose() {
     dio.close();
     super.onClose();
+  }
+
+  void loadMyPageInfo() async {
+    final prettyDioLogger = PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+      compact: true,
+      maxWidth: 80,
+    );
+    isLoading(true);
+    try {
+      dio.interceptors.add(prettyDioLogger);
+      MyPageInfo getmyPageinfo = await UserService(dio).getMyPageInfo(
+        '${AppString.jwt_prefix} ${getString(AppString.key_access_token)!}',
+        getInt(AppString.key_user_id)!,
+      );
+
+      saveString(
+          AppString.key_profile_file_path, getmyPageinfo.profile_file_path!);
+      saveString(AppString.key_nickname, getmyPageinfo.nickname!);
+      saveInt(AppString.key_total_credit, getmyPageinfo.total_credit!);
+      saveInt(
+          AppString.key_this_month_credit, getmyPageinfo.this_month_credit!);
+    } finally {
+      isLoading(false);
+    }
   }
 }
