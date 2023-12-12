bb3e52ac7bec886f6bb35c422a915fbac43884e7
NOGUEN <nhg1113@naver.com>
2023-12-12 14:27:22 +0900

    refactor: 마이페이지에서 데이터를 불러오는 로직을 변경

--- lib/app/controller/my_page_controller.dart
+++ lib/app/controller/my_page_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 import 'package:new_fit/app/data/local/db/storage_util.dart';
 import 'package:new_fit/app/data/model/json_models/mypage/mypage_model.dart';
+import 'package:new_fit/app/services/network_service/user_service.dart';
 import 'package:new_fit/app/view/theme/app_string.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -19,29 +20,54 @@ class MyPageController extends BaseController with StorageUtil {
     maxWidth: 80,
   );
 
-  Rx<MyPageInfo> myPageinfo = MyPageInfo(
-    profile_file_path: 'profile_file_path',
-    nickname: 'nickname',
-    total_credit: 0,
-    this_month_credit: 0,
-    //current: AuthorityGym(authority_id: 0, gym_name: ''),
-    //authority_gyms_count: 0,
-    //authority_gyms: List<AuthorityGym>.empty(),
-  ).obs;
+  final mainFuture = Future.wait([]).obs;
+  final myPageInfo = MyPageInfo().obs;
+  final myPageInfoFuture = Future.value(MyPageInfo()).obs;
 
-  /*
   @override
-  onInit() async {
+  onInit() {
     super.onInit();
-    loadMyPageInfo();
-  }*/
+    updateMainFuture();
+  }
 
-  void loadMyPageInfo() {
-    myPageinfo = MyPageInfo(
-      profile_file_path: getString(AppString.key_profile_file_path)!,
-      nickname: getString(AppString.key_nickname)!,
-      total_credit: getInt(AppString.key_total_credit)!,
-      this_month_credit: getInt(AppString.key_this_month_credit)!,
-    ).obs;
+  updateMainFuture() {
+    getMyPageInfo();
+    mainFuture.value = Future.wait([myPageInfoFuture.value]);
+  }
+
+  assignFutures(List data) {
+    final datas = [myPageInfo];
+    for (var element in data) {
+      datas[data.indexOf(element)].value = element;
+    }
+  }
+
+  void getMyPageInfo() {
+    isLoading(true);
+    try {
+      dio.interceptors.add(prettyDioLogger);
+      myPageInfoFuture.value = UserService(dio).getMyPageInfo(
+        '${AppString.jwt_prefix} ${getString(AppString.key_access_token)!}',
+        getInt(AppString.key_authority_id)!,
+      );
+
+      //saveUserInfo();
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void saveUserInfo() {
+    saveString(
+        AppString.key_profile_file_path, myPageInfo.value.profile_file_path!);
+    saveString(AppString.key_nickname, myPageInfo.value.nickname!);
+    saveInt(AppString.key_total_credit, myPageInfo.value.total_credit!);
+    saveInt(
+        AppString.key_this_month_credit, myPageInfo.value.this_month_credit!);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
