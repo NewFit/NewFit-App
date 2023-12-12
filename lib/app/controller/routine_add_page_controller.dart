ba5ef00bb5624baa3f3e6493562818120edf141a
NOGUEN <nhg1113@naver.com>
2023-12-12 19:52:46 +0900

    feat: routineNameEditingController 추가

--- lib/app/controller/routine_add_page_controller.dart
+++ lib/app/controller/routine_add_page_controller.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/widgets.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
 
-class RoutineAddPageController extends BaseController {}
+class RoutineAddPageController extends BaseController {
+  TextEditingController routineNameEditingController = TextEditingController();
+}
