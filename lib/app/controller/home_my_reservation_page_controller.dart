3aa61b1a77e42f80e02b42452c6fc47fd3b7c089
Haneol <haneolj@gmail.com>
2023-12-12 21:31:53 +0900

    feat: Implement myReservationSpec page and appbar

--- lib/app/controller/home_my_reservation_page_controller.dart
+++ lib/app/controller/home_my_reservation_page_controller.dart
@@ -1,3 +1,107 @@
-import 'package:new_fit/app/core/base/base_controller.dart';
+import 'dart:developer';
 
-class HomeMyReservationPageController extends BaseController {}
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:new_fit/app/core/base/base_controller.dart';
+import 'package:new_fit/app/data/local/db/storage_util.dart';
+import 'package:new_fit/app/data/model/json_models/reservation/reservation_models.dart';
+import 'package:new_fit/app/services/network_service/authority_service.dart';
+import 'package:new_fit/app/services/network_service/reservation_service.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import '../view/theme/app_string.dart';
+
+class HomeMyReservationPageController extends BaseController with StorageUtil {
+  final Dio dio = Dio();
+  late final ReservationService service;
+
+  var reservationList = Rx<ReservationList?>(null);
+  var isLoading = true.obs;
+  var specificReservationList = RxList<SpecificReservation>();
+  var isLoadingSpecs = true.obs;
+
+  final Rx<DateTime> startTime = DateTime(DateTime.now().year,
+          DateTime.now().month, DateTime.now().day, DateTime.now().hour, 0, 0)
+      .obs;
+  final Rx<DateTime> endTime = DateTime(
+          DateTime.now().year,
+          DateTime.now().month,
+          DateTime.now().day,
+          DateTime.now().hour + 2,
+          1,
+          0)
+      .obs;
+
+  @override
+  void onInit() async {
+    super.onInit();
+
+    initService();
+    loadMyReservationList();
+  }
+
+  void initService() {
+    final logger = PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+      compact: true,
+      maxWidth: 80,
+    );
+
+    dio.interceptors.add(logger);
+    service = ReservationService(dio);
+  }
+
+  void loadMyReservationList() async {
+    isLoading(true);
+    try {
+      var now = DateTime.now();
+      startTime.value = DateTime(now.year, now.month, now.day, now.hour, 0, 0);
+      endTime.value =
+          DateTime(now.year, now.month, now.day, now.hour + 2, 1, 0);
+
+      final token =
+          '${AppString.jwt_prefix} ${getString(AppString.key_access_token)}';
+      final authorityId = getInt(AppString.key_authority_id);
+      final gymId = getInt(AppString.key_gym_id)!;
+
+      if (authorityId != null) {
+        log('authority id is $authorityId');
+        var reservation = await service.getReservationList(authorityId, token, gymId);
+        reservationList(reservation);
+      } else {
+        log('ERROR : authority id is null!');
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void loadMyReservationSpecList() async {
+    isLoading(true);
+    try {
+      var now = DateTime.now();
+      startTime.value = DateTime(now.year, now.month, now.day, now.hour, 0, 0);
+      endTime.value =
+          DateTime(now.year, now.month, now.day, now.hour + 2, 1, 0);
+
+      final token =
+          '${AppString.jwt_prefix} ${getString(AppString.key_access_token)}';
+      final authorityId = getInt(AppString.key_authority_id);
+      final gymId = getInt(AppString.key_gym_id)!;
+
+      if (authorityId != null) {
+        log('authority id is $authorityId');
+        var reservation = await service.getReservationList(authorityId, token, gymId);
+        reservationList(reservation);
+      } else {
+        log('ERROR : authority id is null!');
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
--- lib/app/view/main/main_sub_pages/home_my_reservation_page.dart
+++ lib/app/view/main/main_sub_pages/home_my_reservation_page.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:new_fit/app/controller/home_my_reservation_page_controller.dart';
 import 'package:new_fit/app/core/base/base_view.dart';
+import 'package:new_fit/app/data/model/json_models/reservation/reservation_models.dart';
 import 'package:new_fit/app/view/common/base_body.dart';
 
-class HomeMyReservationPage extends BaseView<HomeMyReservationPageController> {
-  @override
-  PreferredSizeWidget? appBar(BuildContext context) {
-    return null;
+import '../../common/newfit_equipment_list.dart';
+
+class HomeMyReservationPage extends StatelessWidget {
+  List<Widget> buildEquipmentList(ReservationList? reservationList) {
+    if (reservationList == null) {
+      return <Widget>[];
+    }
+
+    return List<Widget>.generate(
+      reservationList.reservations.length,
+          (index) {
+        final reservation = reservationList.reservations[index];
+        return NewfitEquipmentListCell(
+          equipmentTitle: reservation.,
+          currentStatus: -1,
+          equipmentId: equipment.equipment_id,
+          equipmentGymId: equipment.equipment_gym_id,
+        );
+      },
+    );
   }
 
   @override
-  Widget body(BuildContext context) {
-    return BaseBody(widgetList: []);
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeMyReservationPageController());
+    return Scaffold(
+      body: BaseBody(widgetList: []),
+    );
   }
 }
