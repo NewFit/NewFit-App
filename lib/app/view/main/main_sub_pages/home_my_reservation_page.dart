9fbb9d3e7f6522f203d638784f82ce58d5be13a3
NOGUEN <65299607+NOGUEN@users.noreply.github.com>
2023-12-15 14:48:20 +0900

    View/qr scanner (#71)
    * refactor: warning sign 제거
    
    * feat: QR코드 페이지 구현
    
    * feat: 메인 페이지에 연결
    
    * feat: QR페이지에서는 앱바가 사라지게 구현
    
    * chore: 카메라 권한 설정을 위한 info.plist 수정

--- lib/app/view/main/main_sub_pages/home_my_reservation_page.dart
+++ lib/app/view/main/main_sub_pages/home_my_reservation_page.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:new_fit/app/controller/home_my_reservation_page_controller.dart';
 import 'package:new_fit/app/controller/home_page_controller.dart';
 import 'package:new_fit/app/core/base/base_view.dart';
