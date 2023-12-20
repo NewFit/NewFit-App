afedb87a7286c27a30d59cdb4ae40d4f17a23ece
NOGUEN <nhg1113@naver.com>
2023-12-20 18:55:46 +0900

    feat: 기능 제안 페이지 구성

---
+++ lib/app/view/setting/setting_sub_pages/feature_sueggest_page.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_fit/app/controller/feature_suggest_page_controller.dart';
+import 'package:new_fit/app/core/base/base_view.dart';
+import 'package:new_fit/app/view/common/base_body.dart';
+import 'package:new_fit/app/view/common/newfit_appbar.dart';
+import 'package:new_fit/app/view/common/newfit_text_field.dart';
+import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_string.dart';
+import 'package:new_fit/app/view/theme/app_text_theme.dart';
+
+class FeatureSuggestPage extends BaseView<FeatureSuggestPageController> {
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return const NewfitAppBarFlat(
+        appBarTitleText: NewfitTextBoldXl(
+      text: AppString.str_feature_suggest_title,
+      textColor: AppColors.black,
+    ));
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return BaseBody(
+      widgetList: [
+        NewfitInfoInputTextField(
+          hintText: '제목',
+          controller: controller.featureNameEditingController,
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        NewfitInfoInputTextField(
+          hintText: '내용',
+          controller: controller.featureNameEditingController,
+        ),
+      ],
+    );
+  }
+}
