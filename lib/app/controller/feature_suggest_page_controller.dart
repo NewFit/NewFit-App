24d82ce5b495ab0f289b109eff6de983c0ea7772
NOGUEN <nhg1113@naver.com>
2023-12-20 19:11:30 +0900

    feat: 기능 제안 제출 메소드 구현

--- lib/app/controller/feature_suggest_page_controller.dart
+++ lib/app/controller/feature_suggest_page_controller.dart
@@ -1,8 +1,40 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:new_fit/app/core/base/base_controller.dart';
+import 'package:new_fit/app/data/local/db/storage_util.dart';
+import 'package:new_fit/app/data/model/json_models/dev/feature_model.dart';
+import 'package:new_fit/app/services/network_service/dev_service.dart';
+import 'package:new_fit/app/view/theme/app_string.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-class FeatureSuggestPageController extends BaseController {
+class FeatureSuggestPageController extends BaseController with StorageUtil {
   TextEditingController featureNameEditingController = TextEditingController();
   TextEditingController featureContentEditingController =
       TextEditingController();
+  Dio dio = Dio();
+  Rx<bool> canSubmit = false.obs;
+
+  final prettyDioLogger = PrettyDioLogger(
+    requestHeader: true,
+    requestBody: true,
+    responseBody: true,
+    responseHeader: true,
+    error: true,
+    compact: true,
+    maxWidth: 500,
+  );
+
+  submitFeatureSuggestion() {
+    Feature feature = Feature(
+        name: featureNameEditingController.text,
+        content: featureContentEditingController.text);
+
+    dio.interceptors.add(prettyDioLogger);
+    DevService(dio).submitFeatureSuggestion(
+      getInt(AppString.key_user_id) ?? 0,
+      '${AppString.jwt_prefix} ${getString(AppString.key_access_token)}',
+      feature,
+    );
+  }
 }
