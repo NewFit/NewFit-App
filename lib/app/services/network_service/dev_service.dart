e2df8cb278be9c79b47bf9e363025d79ab8ef2a2
NOGUEN <nhg1113@naver.com>
2023-12-20 18:40:23 +0900

    feat: DevService, 기능제안 서비스 추가

---
+++ lib/app/services/network_service/dev_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:new_fit/app/data/model/json_models/dev/feature_model.dart';
+import 'package:new_fit/app/data/model/json_models/gym/gym_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'dev_service.g.dart';
+
+@RestApi(
+    baseUrl:
+        "http://ec2-13-209-25-150.ap-northeast-2.compute.amazonaws.com:8080/api/v1/dev")
+abstract class DevService {
+  factory DevService(Dio dio, {String baseUrl}) = _DevService;
+
+  @POST('/feature')
+  Future<AddressGym> getMyGymList(
+    @Header('user-id') int userId,
+    @Header('Authorization') String accessToken,
+    @Body() Feature feature,
+  );
+}
