a537d16201ab6db59eb18ce4574c68b7ba026263
NOGUEN <nhg1113@naver.com>
2023-12-24 01:12:36 +0900

    feat: checkIdTypeAndSave 메소드를 social_login의 메소드로 바꾸어 상속받게 수정

--- lib/app/services/service/social_login.dart
+++ lib/app/services/service/social_login.dart
@@ -1,4 +1,18 @@
-abstract class SocialLogin {
+import 'package:new_fit/app/data/local/db/storage_util.dart';
+import 'package:new_fit/app/data/model/json_models/user/token_model.dart';
+import 'package:new_fit/app/view/theme/app_string.dart';
+
+abstract class SocialLogin with StorageUtil {
   Future<String> login();
   Future<bool> logout();
+
+  void checkIdTypeAndSave(IdInformation idInformation) {
+    if (idInformation.id_type == AppString.key_user_id) {
+      saveInt(AppString.key_user_id, idInformation.id);
+    } else if (idInformation.id_type == AppString.key_oauth_history_id) {
+      saveInt(AppString.key_oauth_history_id, idInformation.id);
+    } else if (idInformation.id_type == AppString.key_authority_id) {
+      saveInt(AppString.key_authority_id, idInformation.id);
+    }
+  }
 }
