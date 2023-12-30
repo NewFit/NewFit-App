042fa0290d31b09b925a5c3db034309e60e3877a
NOGUEN <nhg1113@naver.com>
2023-12-30 21:42:41 +0900

    refactor: 불필요한 코드 제거. NewfitAppBar를 새로운 TabBarAppBar로 수정(클래스명은 그대로 NewfitAppBar 사용). RenewalMainPage를 MainPage로 교체.

--- lib/app/view/common/newfit_appbar.dart
+++ lib/app/view/common/newfit_appbar.dart
@@ -3,208 +3,118 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:new_fit/app/controller/home_my_reservation_page_controller.dart';
-import 'package:new_fit/app/controller/main/main_controller.dart';
+import 'package:new_fit/app/controller/newfit_tab_bar_controller.dart';
 import 'package:new_fit/app/controller/routine_add_page_controller.dart';
-import 'package:new_fit/app/data/local/db/storage_util.dart';
 import 'package:new_fit/app/data/model/enum/menu_code.dart';
-import 'package:new_fit/app/data/model/json_models/reservation/reservation_models.dart';
+import 'package:new_fit/app/data/model/menu/menu_item.dart';
 import 'package:new_fit/app/routes/app_pages.dart';
-import 'package:new_fit/app/view/common/newfit_button.dart';
-import 'package:new_fit/app/view/common/newfit_progressbar.dart';
 import 'package:new_fit/app/view/common/newfit_text_field.dart';
 import 'package:new_fit/app/view/theme/app_colors.dart';
+import 'package:new_fit/app/view/theme/app_fontweight.dart';
 import 'package:new_fit/app/view/theme/app_string.dart';
 import 'package:new_fit/app/view/theme/app_text_theme.dart';
 import 'package:new_fit/app/view/theme/app_values.dart';
 
-import 'newfit_schedule.dart';
-
-class NewfitAppBar extends StatelessWidget
-    with StorageUtil
-    implements PreferredSizeWidget {
-  NewfitAppBar({
-    required this.mainController,
-    required this.scrollController,
+class NewfitAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const NewfitAppBar({
     super.key,
+    required this.tabController,
+    required this.tabBarController,
   });
 
-  MainController mainController;
-  ScrollController scrollController;
-  Rx<double> scrollPosition = 0.0.obs;
-  double appBarHeight = 183.h;
+  final TabController tabController;
+  final TabBarController tabBarController;
 
   @override
   Widget build(BuildContext context) {
-    Widget replaceWidget = homeAppBar();
-    scrollController.addListener(_scrollListener);
+    List<TabBarItem> tabItemList = _getTabBarItems();
 
     return Obx(
-      () {
-        if (mainController.selectedMenuCode == MenuCode.HOME) {
-          replaceWidget = HomeAppBar(scrollPosition: scrollPosition.value);
-          if (scrollPosition.value > 0.0) {
-            appBarHeight = 105.h + MediaQuery.of(context).padding.top;
-          } else {
-            appBarHeight = 172.h + MediaQuery.of(context).padding.top;
-          }
-        } else if (mainController.selectedMenuCode == MenuCode.SCOREBOARD) {
-          replaceWidget = const SafeArea(
-            child: Center(
-              child: NewfitTextBoldXl(
-                text: AppString.str_scoreboard_title,
-                textColor: AppColors.black,
-              ),
-            ),
-          );
-          appBarHeight = 50.h + MediaQuery.of(context).padding.top;
-        } else if (mainController.selectedMenuCode == MenuCode.RESERVE) {
-          replaceWidget = myReservationAppBar();
-          appBarHeight = 160.h + MediaQuery.of(context).padding.top;
-        } else if (mainController.selectedMenuCode == MenuCode.QR) {
-          return const SizedBox();
-        }
-        return Container(
-          height: appBarHeight,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(16.r),
-              bottomRight: Radius.circular(16.r),
-            ),
-            border: Border.all(color: AppColors.grayDisabled),
-            color: AppColors.white,
+      () => Container(
+        height: 90.h + MediaQuery.of(context).padding.top,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(4.r),
+            bottomRight: Radius.circular(4.r),
           ),
-          child: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 0),
-              child: replaceWidget,
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).padding.top + 10.h),
+            Row(
+              children: [
+                SizedBox(width: 20.w),
+                const NewfitTextBold2Xl(
+                    text: AppString.str_app_title_kor,
+                    textColor: AppColors.black),
+                const Spacer(),
+                GestureDetector(
+                  child: const CircleAvatar(),
+                  onTap: () {
+                    Get.toNamed(AppPages.MY);
+                  },
+                ),
+                SizedBox(width: 20.w),
+              ],
             ),
-          ),
-        );
-      },
+            const Spacer(),
+            TabBar(
+              controller: tabController,
+              tabs: tabItemList
+                  .map(
+                    (TabBarItem tabItem) => Tab(
+                      child: Text(
+                        tabItem.tabBarTitle,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: tabItemList.indexOf(tabItem) ==
+                                  tabBarController.selectedIndex
+                              ? AppColors.black
+                              : AppColors.textUnabled,
+                          fontWeight: tabItemList.indexOf(tabItem) ==
+                                  tabBarController.selectedIndex
+                              ? AppFontWeights.bold
+                              : AppFontWeights.medium,
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+              onTap: (index) {
+                tabBarController.updateSelectedIndex(index);
+              },
+              indicatorColor: Colors.transparent,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget homeAppBar() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 13.h),
-        _UserInfoAppBar(
-          userName: getString(AppString.key_nickname) ?? 'NULL',
-          onPressedFunction: () {
-            Get.toNamed(AppPages.SETTING);
-          },
-        ),
-        if (scrollPosition.value <= 0.0) SizedBox(height: 10.h),
-        if (scrollPosition.value <= 0.0)
-          _UserCreditInfo(
-            totalCredit: getInt(AppString.key_total_credit) ?? 0,
-            todayCredit: getInt(AppString.key_this_month_credit) ?? 0,
-          ),
-        SizedBox(height: 10.h),
-        Align(
-          alignment: Alignment.center,
-          child: NewfitButton(
-              buttonText: AppString.button_reservation_with_routine,
-              buttonColor: AppColors.main,
-              onPressFuntion: () {}),
-        ),
-      ],
-    );
-  }
-
-  Widget myReservationAppBar() {
-    final HomeMyReservationPageController myReservationPageController =
-        Get.find();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 13.h),
-        _UserInfoAppBar(
-          userName: getString(AppString.key_nickname)!,
-          onPressedFunction: () {
-            Get.toNamed(AppPages.SETTING);
-          },
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
-          child: Obx(() {
-            if (myReservationPageController.isLoading.value) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return NewfitSchedule(
-              scheduleList: List.generate(
-                  myReservationPageController
-                      .reservationList.value.reservations.length,
-                  (index) => Reservation(
-                      start_at: myReservationPageController
-                          .reservationList.value.reservations[index].start_at,
-                      end_at: myReservationPageController
-                          .reservationList.value.reservations[index].end_at)),
-              startTime: myReservationPageController.startTime.value,
-              endTime: myReservationPageController.endTime.value,
-              selectedIndex: myReservationPageController.selectedIndex.value,
-            );
-          }),
-        ),
-        SizedBox(height: 5.h),
-        Align(
-          alignment: Alignment.center,
-          child: NewfitButton(
-              buttonText: AppString.button_reservation_with_routine,
-              buttonColor: AppColors.main,
-              onPressFuntion: () {}),
-        ),
-      ],
-    );
-  }
-
-  _scrollListener() {
-    scrollPosition.value = scrollController.offset;
+  List<TabBarItem> _getTabBarItems() {
+    return [
+      const TabBarItem(
+        tabBarTitle: AppString.tabbar_home,
+        menuCode: MenuCode.HOME,
+      ),
+      const TabBarItem(
+        tabBarTitle: AppString.tabbar_reserve,
+        menuCode: MenuCode.RESERVE,
+      ),
+      const TabBarItem(
+        tabBarTitle: AppString.tabbar_qr,
+        menuCode: MenuCode.QR,
+      ),
+      const TabBarItem(
+        tabBarTitle: AppString.tabbar_scoreboard,
+        menuCode: MenuCode.SCOREBOARD,
+      ),
+    ];
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(appBarHeight);
-}
-
-class HomeAppBar extends StatelessWidget with StorageUtil {
-  HomeAppBar({
-    required this.scrollPosition,
-    super.key,
-  });
-
-  final double scrollPosition;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 13.h),
-        _UserInfoAppBar(
-          userName: getString(AppString.key_nickname) ?? 'NULL',
-          onPressedFunction: () {
-            Get.toNamed(AppPages.SETTING);
-          },
-        ),
-        if (scrollPosition <= 0.0) SizedBox(height: 10.h),
-        if (scrollPosition <= 0.0)
-          _UserCreditInfo(
-            totalCredit: getInt(AppString.key_total_credit) ?? 0,
-            todayCredit: getInt(AppString.key_this_month_credit) ?? 0,
-          ),
-        SizedBox(height: 10.h),
-        Align(
-          alignment: Alignment.center,
-          child: NewfitButton(
-              buttonText: AppString.button_reservation_with_routine,
-              buttonColor: AppColors.main,
-              onPressFuntion: () {}),
-        ),
-      ],
-    );
-  }
+  Size get preferredSize => Size.fromHeight(100.h);
 }
 
 class NewfitAppBarElevated extends StatelessWidget
@@ -393,164 +303,6 @@ class NewfitAppBarTranparent extends StatelessWidget
 
   _scrollListener() {
     scrollPosition.value = scrollController.offset;
-  }
-}
-
-class _HomeAppBar extends StatelessWidget {
-  const _HomeAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-class _UserInfoAppBar extends StatelessWidget with StorageUtil {
-  _UserInfoAppBar({
-    required this.userName,
-    required this.onPressedFunction,
-    super.key,
-  });
-
-  final String userName;
-  final Function()? onPressedFunction;
-
-  ImageProvider<Object>? getProfileImage() {
-    String? imageUrl = getString(AppString.key_profile_file_path);
-
-    if (imageUrl != null && isValidUrl(imageUrl)) {
-      try {
-        return NetworkImage(imageUrl);
-      } catch (e) {
-        return const AssetImage(AppString.gorani);
-      }
-    } else {
-      return const AssetImage(AppString.gorani);
-    }
-  }
-
-  bool isValidUrl(String url) {
-    var pattern = r'^(?:http|ftp)s?://' // http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+' // domain
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' // domain name
-        r'localhost|' // localhost
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' // ...or ipv4
-        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' // ...or ipv6
-        r'(?::\d+)?' // optional port
-        r'(?:/?|[/?]\S+)$';
-    RegExp regExp = RegExp(pattern, caseSensitive: false);
-
-    return regExp.hasMatch(url);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        GestureDetector(
-            child: Container(
-              height: 40.h,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.r),
-                  color: AppColors.white,
-                  border: Border.all(
-                    color: AppColors.grayDisabled,
-                  )),
-              child: Row(children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 7.w),
-                  child: CircleAvatar(
-                    backgroundColor: AppColors.main,
-                    radius: 15.5.h,
-                    child: CircleAvatar(
-                      radius: 14.h,
-                      foregroundImage: getProfileImage(),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10.w),
-                Padding(
-                  padding: EdgeInsets.only(right: 7.w),
-                  child: Row(children: [
-                    NewfitTextBoldXl(
-                        text: "$userName님", textColor: AppColors.black),
-                    SizedBox(
-                      width: 3.w,
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: AppColors.grayDisabled,
-                      size: 16.w,
-                    ),
-                  ]),
-                )
-              ]),
-            ),
-            onTap: () {
-              Get.toNamed(AppPages.MY);
-            }),
-        const Spacer(),
-        GestureDetector(
-          onTap: onPressedFunction,
-          child: const Icon(
-            Icons.settings,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class _UserCreditInfo extends StatelessWidget {
-  const _UserCreditInfo({
-    required this.totalCredit,
-    required this.todayCredit,
-  });
-
-  final int totalCredit;
-  final int todayCredit;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            const NewfitTextMediumMd(
-              text: AppString.str_total_credit,
-              textColor: AppColors.black,
-            ),
-            SizedBox(width: 5.w),
-            NewfitTextRegularMd(
-              text: '$totalCredit',
-              textColor: AppColors.main,
-            ),
-          ],
-        ),
-        SizedBox(height: 5.h),
-        Row(
-          children: [
-            const NewfitTextMediumMd(
-              text: AppString.str_today_credit,
-              textColor: AppColors.black,
-            ),
-            SizedBox(width: 5.w),
-            NewfitTextRegularMd(
-              text: '$todayCredit',
-              textColor: AppColors.main,
-            ),
-          ],
-        ),
-        SizedBox(height: 10.h),
-        Align(
-          alignment: Alignment.center,
-          child: NewfitProgressBar(
-            progressBarHeight: 4.h,
-            progressBarValue: todayCredit / 100,
-          ),
-        ),
-      ],
-    );
   }
 }
 
