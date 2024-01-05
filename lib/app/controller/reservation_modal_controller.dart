46472569315ce59838d8dd81dd7ec4bc4290af0c
NOGUEN <nhg1113@naver.com>
2024-01-05 23:21:17 +0900

    feat: 예약 메소드 추가

--- lib/app/controller/reservation_modal_controller.dart
+++ lib/app/controller/reservation_modal_controller.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:new_fit/app/controller/home_my_reservation_page_controller.dart';
 import 'package:new_fit/app/data/local/db/storage_util.dart';
 import 'package:new_fit/app/data/model/json_models/equipment/equipment_models.dart';
+import 'package:new_fit/app/data/model/json_models/reservation/reservation_models.dart';
 import 'package:new_fit/app/services/network_service/equipment_service.dart';
+import 'package:new_fit/app/services/network_service/reservation_service.dart';
 import 'package:new_fit/app/view/theme/app_string.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -37,6 +41,8 @@ class ReservationModalController with StorageUtil {
   );
 
   TextEditingController textEditingController = TextEditingController();
+  final HomeMyReservationPageController _myReservationPageController =
+      Get.find();
 
   var equipmentList = Rx<EquipmentList?>(null);
   var equipmentSpec = Rx<EquipmentSpec?>(null);
@@ -111,5 +117,33 @@ class ReservationModalController with StorageUtil {
     selectedIndex(index);
     equipmentGymId = indexMap[index];
     loadEquipmentSpecification();
+  }
+
+  void equipmentReservation() async {
+    final start =
+        DateFormat(AppString.date_format_hh_mm).format(chosenStartTime.value);
+    final end =
+        DateFormat(AppString.date_format_hh_mm).format(chosenEndTime.value);
+
+    try {
+      final token =
+          '${AppString.jwt_prefix} ${getString(AppString.key_access_token)}';
+      final authorityId = getInt(AppString.key_authority_id);
+
+      if (authorityId != null) {
+        ReservationService(dio).reserveEquipment(
+            authorityId,
+            token,
+            equipmentGymId,
+            Reservation(start_at: startTime.value, end_at: endTime.value));
+
+        await Future.delayed(const Duration(milliseconds: 300));
+        _myReservationPageController.specificReservationList.clear();
+        _myReservationPageController.loadMyReservationList();
+      } else {}
+    } catch (e) {}
+
+    Get.back();
+    Get.snackbar(AppString.snackbar_check_reservation, '$start ~ $end');
   }
 }
